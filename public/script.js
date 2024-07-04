d25a0b534c4a8a9ddfc5ba77bf0ea7507bc9cc14
Arsh Gupta <99011038+ArshGupta74@users.noreply.github.com>
2024-07-05 03:21:06 +0530

    Update script.js

--- public/script.js
+++ public/script.js
@@ -4,7 +4,32 @@ function updateSensorData() {
     .then(data => {
         console.log('Received data:', data);
         document.getElementById('turbidity').textContent = data.turbidity !== undefined ? data.turbidity : 'N/A';
-        document.getElementById('distance').textContent = data.distance !== undefined ? data.distance + ' cm' : 'N/A';
+        
+        const distanceElement = document.getElementById('distance');
+        const pumpStatusElement = document.getElementById('pump-status');
+
+        console.log('Distance:', data.distance);
+        
+        if (data.distance !== undefined) {
+            distanceElement.textContent = data.distance + ' cm';
+            
+            // Update pump status based on distance
+            if (data.distance > 50) {
+                pumpStatusElement.textContent = 'Pump is running';
+                pumpStatusElement.style.color = 'black';
+                pumpStatusElement.style.backgroundColor = '#83f28f';
+                pumpStatusElement.style.fontWeight = 'bold';
+            } else {
+                pumpStatusElement.textContent = 'Pump is not running';
+                pumpStatusElement.style.fontWeight = 'bold';
+                pumpStatusElement.style.color = 'black';
+                pumpStatusElement.style.backgroundColor = '#f94449';
+            }
+        } else {
+            distanceElement.textContent = 'N/A';
+            pumpStatusElement.textContent = 'Unknown';
+            pumpStatusElement.style.color = 'gray';
+        }
     })
     .catch(error => {
         console.error('Error fetching sensor data:', error);
